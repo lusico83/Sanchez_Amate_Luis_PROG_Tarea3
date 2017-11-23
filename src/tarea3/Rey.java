3c64f3129afea666084422f0fcecb125db1709bc
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2017-11-23 21:41:50 +0100

    Creacion Clase Rey

---
+++ src/tarea3/Rey.java
@@ -0,0 +1,20 @@
+
+package tarea3;
+
+public class Rey {
+    
+    private Color color_rey;
+    private Posicion posicion_rey;
+    
+    
+    
+            
+            
+    
+    
+    
+    
+    
+    
+    
+}
