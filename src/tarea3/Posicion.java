d0230830d8f70b904ebb375d0eb2811925feedb6
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2017-11-23 21:16:59 +0100

    Creacion del constructor de Posicion

--- src/tarea3/Posicion.java
+++ src/tarea3/Posicion.java
@@ -6,5 +6,27 @@ public class Posicion {
     private int fila;
     private char columna;
     
+    //Constructor  
+    
+    Posicion(int fila, char columna){
+    
+    if(fila>0&&fila<9){
+    this.fila=fila;
+    }
+    else{
+    System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
+    this.fila=1;
+    }
+    
+    if(columna>='a'&&columna<='h'){
+    this.columna=columna;
+    }
+    else{
+    System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
+    this.columna='a';
+    }
+    
+    }
+    
     
 }
