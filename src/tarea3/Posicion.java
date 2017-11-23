a28359811ea27b2a5d1eef70569e5fae699578cf
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2017-11-23 21:54:26 +0100

    Creacion Constructor Rey

--- src/tarea3/Posicion.java
+++ src/tarea3/Posicion.java
@@ -3,28 +3,15 @@ package tarea3;
 
 public class Posicion {
     
-    private int fila;
-    private char columna;
+    public int fila;
+    public char columna;
     
     //Constructor  
     
     Posicion(int fila, char columna){
-    
-    if(fila>0&&fila<9){
+        
     this.fila=fila;
-    }
-    else{
-    System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
-    this.fila=1;
-    }
-    
-    if(columna>='a'&&columna<='h'){
     this.columna=columna;
-    }
-    else{
-    System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
-    this.columna='a';
-    }
     
     }
     
--- src/tarea3/Rey.java
+++ src/tarea3/Rey.java
@@ -6,6 +6,21 @@ public class Rey {
     private Color color_rey;
     private Posicion posicion_rey;
     
+
+    Rey(enum color){
+            
+    this.color=color;
+    
+    if(color='BLANCO'){
+        posicion_rey.fila=1;
+        posicion_rey.columna='e';
+    }else{
+        posicion_rey.fila=8;
+        posicion_rey.columna='e';    
+        }
+
+}
+    
     
     
             
