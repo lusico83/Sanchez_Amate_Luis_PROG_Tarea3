09a553ae55435383772f6ddda08dc167bb37f848
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2017-11-23 21:25:05 +0100

    Creacion de los metodos SET y GET de Posicion

--- src/tarea3/Posicion.java
+++ src/tarea3/Posicion.java
@@ -28,5 +28,47 @@ public class Posicion {
     
     }
     
+    //Metodos Get y Set
+    
+    public void setFila(int fila){
+        
+        if(fila>0&&fila<9){
+        this.fila=fila;
+    }
+    else{
+        System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
+        this.fila=1;
+    }
+    
+    }
+    
+    public void setColumna(char columna){
+        
+        if(columna>='a'&&columna<='h'){
+        this.columna=columna;
+    }
+    else{
+        System.out.println("Fila erronea. Cogeremos por defecto la fila 1.");
+        this.columna='a';
+    } 
+    
+    }
+    
+    public int getFila(){
+        
+        return fila;
+        
+    }
+    
+    public char getColumna(){
+        
+        return columna;
+        
+    }
+    
+    
+    
+    
+    
     
 }
