30a9a374c9d7ca0d39c54640ccc7f971983daf92
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2017-11-23 21:04:05 +0100

    Creacion del Enum Direccion

---
+++ src/tarea3/Direccion.java
@@ -0,0 +1,16 @@
+//En el caso de direccion, almacenamos las posibilidades 
+
+package tarea3;
+
+public enum Direccion {
+    
+    NORTE, 
+    NORESTE, 
+    ESTE, 
+    SURESTE, 
+    SUR, 
+    SUROESTE, 
+    OESTE,
+    NOROESTE;
+    
+}
